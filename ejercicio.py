1c9dc963b8ad143472e81200f5db07d030f665e4
barc1023 <arodriguezbryan57@gmail.com>
2024-09-07 15:28:27 -0500

    Add files via upload
    Carga Inicial

---
+++ ejercicio.py
@@ -0,0 +1,15 @@
+sujeto = input("sea bienvenido, podria indicarme su nombre")
+numero_mayor = 34
+numero_pequeño = 8
+
+sumar = numero_mayor + numero_pequeño
+restar = numero_mayor - numero_pequeño
+dividir = numero_mayor / numero_pequeño
+multiplicar = numero_mayor * numero_pequeño
+
+print(f" {sujeto}")
+print(f"la suma de {numero_mayor} + {numero_pequeño} es {sumar}")
+print(f"la resta de {numero_mayor} - {numero_pequeño} es {restar}")
+print(f"la division de {numero_mayor} / {numero_pequeño} es {dividir}")
+print(f"la multiplicacion de {numero_mayor} * {numero_pequeño} es {multiplicar} ")
+
---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+sujeto = input("¡buenas tardes! sea bienvenido a nuestra sucursal, puede indicarme su nombre")
+producto_promocionado = "perfume de rosas"
+cantidad = 1
+precio = 45.99
+
+print(f"¡hola {sujeto}!, su {producto_promocionado}, tiene un precio total de {precio} por la cantidad de {cantidad}, ¡ESTAMOS AGRADECIDOS POR SU COMPRA!")
