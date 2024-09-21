19aa3e9a0bd2e279fd3d7c4fc34ab603241e16b4
barc1023 <arodriguezbryan57@gmail.com>
2024-09-20 21:01:16 -0500

    clase del 20 de septiembre

---
+++ modulo 4/clase 16/entorno_de_desarrollo.py
@@ -0,0 +1,29 @@
+'''
+ IDE:
+
+ es un entorno de desarrollo integrado. cuenta con un: un editor de estudio, permite escribir y modificar codigos
+ tambien tiene un depurador, interpetre, gestion de prollectos, organizar modulos y prollectos,integracion con versiones del proyecto, autocompletado,
+ tiene funcionalidades de refacturacion.
+
+'''
+'''
+editor de codigo:
+
+se utiliza para editar codigos sencillos
+
+'''
+'''
+ IDEs y editores de texto:
+
+19:26
+PyCharm (para Python)
+Visual Studio (para múltiples lenguajes)
+IntelliJ IDEA (para Java)
+Eclipse (para Java, C++ y otros lenguajes)
+Xcode (para desarrollo de aplicaciones en macOS y iOS)
+
+Sublime Text
+Notepad++
+Atom
+
+'''
